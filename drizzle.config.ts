07a9ed3bbca347aad219e1de9c93b620594c405a
Faiyaz Sarkar <faiyazvai999@gmail.com>
2025-11-16 12:33:03 +0600

    initial clerk setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,10 +2,8 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-export default defineConfig({
   out: "./drizzle/migrations",
   schema: "./drizzle/schema.ts",
-  dialect: "postgresql",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL as string,
